49a6ad2efcc9a8971b18c5608830deadb6fe56c6
karina77478 <bordakari9@gmail.com>
2023-07-18 10:54:53 -0500

    ultimo

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import C01componente from "./components/C01componente";
+import C02contador from "./components/C02contador";
+import C03doblecontador from "./components/C03doblecontador";
+
+
+function App() {
+  return (
+    <div>
+      <C01componente/>
+
+      <hr/>
+      <C02contador/>
+
+      <hr/>
+      <C03doblecontador/>
+
+
+      
+    </div>
+  );
+}
+
+export default App;
